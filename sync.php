e090bc3748092c3af31fff0bd6d6d24f0ee87a59
itsaraphap <itsaraphap.t@gmail.com>
2020-08-13 09:09:11 +0700

    eee

--- sync.php
+++ sync.php
@@ -4,16 +4,18 @@ include('connect.php');
 $memberID =  $_GET['memberID'];
 $userID =  $_GET['userID'];
 $sql = "update member set lineid = '".$_GET['userID']."' WHERE m_code='".$_GET['memberID']."'";
-$query = mysqli_query($conn, $sql);
-mysqli_close($conn);
-if ($memberID=="") {
-    echo "No Select Member!";
-}else{
-    echo "Sync Success";
-}
-// $json = array();
-// while($result = mysqli_fetch_assoc($query)) {    
-//     array_push($json, $result);
+// $query = mysqli_query($conn, $sql);
+// mysqli_close($conn);
+// if ($memberID=="") {
+//     echo "No Select Member!";
+// }else{
+//     echo "Sync Success";
 // }
-// echo json_encode($json);
+$sql = "select * from member where m_code='".$_GET['memberID']."'";
+$query = mysqli_query($conn, $sql);
+$json = array();
+while($result = mysqli_fetch_assoc($query)) {    
+    array_push($json, $result);
+}
+echo json_encode($json);
 ?>
