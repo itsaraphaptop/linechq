1f0d35abff2a493d2a1d436dfc83bcaf174af5f3
itsaraphap <itsaraphap.t@gmail.com>
2020-08-18 23:26:14 +0700

    ddd

--- sync.php
+++ sync.php
@@ -4,8 +4,7 @@ include('connect.php');
 $username =  $_GET['uName'];
 $password =  $_GET['passWord'];
 $userID =  $_GET['userID'];
-// $sql = "update member set lineid = '".$_GET['userID']."' WHERE m_code='".$_GET['memberID']."'";
-// $query = mysqli_query($conn, $sql);
+
 // mysqli_close($conn);
 // if ($memberID=="") {
 //     echo "No Select Member!";
@@ -15,6 +14,8 @@ $userID =  $_GET['userID'];
 $sqla = "select m_user,m_pass from member where m_user='".$username."' and m_pass='".sha1(sha1(md5($password)))."'";
 $querya = mysqli_query($conn, $sqla);
 if (mysqli_num_rows($querya) > 0) {
+    $sql = "update member set lineid = '".$userID."' WHERE m_user='".$username."'";
+    $query = mysqli_query($conn, $sql);
     // $jsonq = array();
     // while($resultq = mysqli_fetch_assoc($querya)) {    
     //     array_push($jsonq, $resultq);
