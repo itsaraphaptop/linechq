96099a3b495ae93a2d508be7a6e4e8b231bda51f
itsaraphap <itsaraphap.t@gmail.com>
2020-08-21 11:18:08 +0700

    .

--- botpush.php
+++ botpush.php
@@ -110,7 +110,7 @@
           // $arrayPostData['messages'][0]['contents']['footer']['contents'][0]['action']['uri'] =  $request_array['base_url'];
           
           
-           var_dump($request_array);
+           json_decode($request_array);
           // die();
       pushMsg($arrayHeader,$arrayPostData);
       echo true;
